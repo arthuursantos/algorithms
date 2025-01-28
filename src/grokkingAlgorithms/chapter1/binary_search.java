31a93869a335812e039df56b4717d2e66f87477e
Arthur Santos <arthur.saantos20@gmail.com>
2025-01-28 19:31:12 -0300

    Grokking Algorithms, Aditya H. Bhargava
    Troquei o livro do Cormen por esse. É mais introdutório. Hoje li sobre
    pesquisa binária, o primeiro algoritmo que o livro apresenta.

---
+++ src/grokkingAlgorithms/chapter1/binary_search.java
@@ -0,0 +1,36 @@
+package grokkingAlgorithms.chapter1;
+
+public class binary_search {
+
+    public static Integer search(int[] array, int target) {
+        int first = 0;
+        int last = array.length - 1;
+        while (first <= last) {
+            int mid = (first + last) / 2;
+            if (array[mid]==target) {
+                return mid;
+            } else if (array[mid]<target) {
+                first = mid+1;
+            } else {
+                last = mid-1;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2, 12, 24, 47, 57, 84}; // o array precisa estar ordenado
+        System.out.println("index " + search(array, 84));;
+    }
+
+    // 1.1 Suponha que você tenha uma lista com 128 nomes e esteja a fazer uma
+    // pesquisa binária. Qual seria o número máximo de etapas que você levaria
+    // para encontrar o nome desejado?
+    // R: log 128 = 7
+
+    // 1.2 Suponha que você duplique o tamanho da lista. Qual seria o número
+    // máximo de etapas agora?
+    // R: log 256 = 8
+
+
+}
