bc932ca6371a04f20028e044275920cf1baf02ba
Arthur Santos <arthur.saantos20@gmail.com>
2025-02-26 16:49:14 -0300

    Grokking Algorithms, Aditya Y. Bhargava
    Compreendi o algoritmo de selection sort apresentado no capítulo.

---
+++ src/grokkingAlgorithms/chapter2/selection_sort.java
@@ -0,0 +1,33 @@
+package grokkingAlgorithms.chapter2;
+
+import java.util.Arrays;
+
+public class selection_sort {
+
+    public static int findBiggestValue(int[] array, int i) {
+        int bIndex = i;
+        for (int j = i+1; j < array.length; j++) {
+            if (array[j] > array[bIndex]) {
+                bIndex = j;
+            }
+        }
+        return bIndex;
+    }
+
+    public static int[] selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int bIndex = findBiggestValue(array, i);
+            int bValue = array[bIndex];
+            for (int k = bIndex; k > i; k--) {
+                array[k] = array[k-1];
+            }
+            array[i] = bValue;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] orderedArray = selectionSort(new int[]{1, 12, 32, 96, 10, 6, 0});
+        System.out.println(Arrays.toString(orderedArray));
+    }
+}
